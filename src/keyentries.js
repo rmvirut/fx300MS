c538693510fa50c4d3e88a7841f1223a1ccdc4a3
Kojo Acquah <kojo.acquah@hotmail.com>
2016-09-16 14:56:11 -0300

    Uploaded Projects To Repo
    Created this project prior to learning github and I finally decided to upload it here and work on it to improve my understanding of version control.

---
+++ src/keyentries.js
@@ -0,0 +1,11 @@
+var charString="_";
+var arithmetic_signs = "+-*/";
+
+function charEntry(character) {
+    charString = character + charString;
+    document.getElementById("equation_field").innerHTML = charString;
+} 
+
+/*still working on reading keyboard enries*/
+
+
